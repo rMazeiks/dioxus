e1106f6407aab7c059fc0442ac7ad7848f63bd2b
Reinis Mazeiks <reinis.mazeiks@gmail.com>
2022-05-11 12:07:37 +0300

    replace macro with direct impl

--- packages/html/src/elements.rs
+++ packages/html/src/elements.rs
@@ -1,29 +1,13 @@
-use crate::{GlobalAttributes, SvgAttributes};
 use dioxus_core::*;
 use std::fmt::Arguments;
 
-macro_rules! builder_constructors {
-    (
-        $(
-            $name:ident {
-            };
-         )*
-    ) => {
-        $(
-            pub struct $name;
+pub struct input;
 
-            impl DioxusElement for $name {
-                const TAG_NAME: &'static str = stringify!($name);
-                const NAME_SPACE: Option<&'static str> = None;
-            }
-        )*
-    };
+impl DioxusElement for input {
+    const TAG_NAME: &'static str = "input";
+    const NAME_SPACE: Option<&'static str> = None;
 }
 
-builder_constructors! {
-    input {
-    };
-}
 impl input {
     pub fn r#type<'a>(&self, cx: NodeFactory<'a>, val: Arguments) -> Attribute<'a> {
         loop {}
