0619cb532f8a718566833aa8f2414e5f6fe6bcfe
Reinis Mazeiks <reinis.mazeiks@gmail.com>
2022-05-11 13:07:05 +0300

    delete macro crate

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,5 @@ pub use dioxus_desktop as desktop;
 
 pub mod prelude {
     pub use dioxus_core::prelude::*;
-    pub use dioxus_core_macro::{format_args_f, inline_props, rsx, Props};
     pub use dioxus_html as dioxus_elements;
 }
